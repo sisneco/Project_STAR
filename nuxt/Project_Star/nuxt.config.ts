404971e2f27bf820d8ef630d902f545a1d4bcc88
sisneco <sis.cat64@gmail.com>
2023-08-22 22:48:16 +0900

    first commit

---
+++ nuxt/Project_Star/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  routeRules: {
+    "/login": { ssr: true, prerender: false },
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      link: [
+        { rel: "icon", href: "/icon.png" },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@200&display=swap",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap",
+        },
+      ],
+    },
+  },
+});
---
+++ nuxt/Project_Star/pages/login.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="w-screen h-screen flex justify-center items-center">
+    <div class="">
+      <div class="w-[600px] h-[860px] bg-red-100"></div>
+    </div>
+  </main>
+</template>
