c3f1d76bc37c12f4549b91313f481895daae497f
sisneco <sis.cat64@gmail.com>
2023-10-03 23:17:43 +0900

    ホーム画面を更新（フォーム作成）
    TODO：コンポーネント化

--- nuxt/Project_Star/pages/home.vue
+++ nuxt/Project_Star/pages/home.vue
@@ -3,8 +3,34 @@
 
 // values
 const message: Ref = ref("");
+const btnText: Ref = ref("次へ");
 
 const HEIGHT_WRAPPER_TEXTAREA = 150;
+
+const formArray: Array<Form> = new Array();
+
+class Form {
+  key: string;
+  placeholderText: string;
+  value: string;
+
+  constructor(key: string, placeholder: string) {
+    this.key = key;
+    this.placeholderText = placeholder;
+
+    // valueのみ空白で初期化
+    this.value = "";
+  }
+}
+
+const currentForm: Ref<Form> = ref(
+  new Form("wantItem", "今欲しいものを書き込んでみましょう！")
+);
+
+formArray.push(currentForm.value);
+formArray.push(new Form("price", "値段はどれぐらいになりそうですか？"));
+formArray.push(new Form("priority", "優先度はどうでしょうか？"));
+formArray.push(new Form("memo", "なにか要望などがあれば書いてみましょう！"));
 
 // methods
 /**
@@ -56,6 +82,55 @@ function countParamNewLine(target: string): number {
   const cnt: any = target.match(regExp)?.length;
   return cnt === undefined ? 0 : cnt;
 }
+
+/**
+ * 次へ or 登録押下時のボタン処理
+ */
+function btnNextAction() {
+  btnCommonAction(true);
+}
+
+/**
+ * 戻るボタン押下時のボタン処理
+ */
+function btnBackAction() {
+  btnCommonAction(false);
+}
+
+/**
+ * ボタン共通処理
+ * @param isNext true 次へボタン false 戻るボタン
+ */
+function btnCommonAction(isNext: boolean) {
+  // 現在のkeyの添字を取得
+  let index: number = nowFormArrayIndex.value;
+  // 入力値を保存
+  formArray[index].value = message.value;
+
+  // 次へボタンの場合 -> インクリメント , 戻るボタンの場合 -> デクリメント
+  index = isNext ? ++index : --index;
+
+  // 項目を更新(上記処理により、該当するフォームの値に紐づいて更新)
+  currentForm.value = formArray[index];
+  message.value = currentForm.value.value;
+
+  // ボタンテキストを更新
+  btnText.value = index === formArray.length - 1 ? "登録" : "次へ";
+}
+
+// computed
+const isShowBtnBack = computed(() => {
+  /** 添字(一番初め) */
+  const FIRST_INDEX: number = 0;
+
+  return (
+    formArray.findIndex((v) => v.key === currentForm.value.key) !== FIRST_INDEX
+  );
+});
+
+const nowFormArrayIndex = computed(() => {
+  return formArray.findIndex((v) => v.key === currentForm.value.key);
+});
 </script>
 
 <template>
@@ -69,13 +144,27 @@ function countParamNewLine(target: string): number {
       >
         <textarea
           v-model="message"
-          placeholder="今欲しいものを書き込んでみましょう！"
+          :placeholder="currentForm.placeholderText"
           class="resize-none w-full h-[100px] outline-none p-4 text-xl overflow-hidden"
           @input="autoResizeTextArea($event)"
         ></textarea>
-        <button class="bg-blue-500 w-32 rounded-full p-2 ml-auto mr-2 h-[50px]">
-          優先度は？
-        </button>
+        <div
+          class="w-full rounded-full h-[45px] flex justify-end gap-x-2 pr-4 text-white font-bold font-sans"
+        >
+          <button
+            class="w-32 rounded-full p-2 h-full text-gray-600 font-bold font-sans border border-gray-600"
+            @click="btnBackAction()"
+            v-if="isShowBtnBack"
+          >
+            戻る
+          </button>
+          <button
+            class="bg-blue-500 w-32 rounded-full p-2 h-full text-white font-bold font-sans"
+            @click="btnNextAction()"
+          >
+            {{ btnText }}
+          </button>
+        </div>
       </div>
       <div class="bg-red-200 w-full p-8 flex flex-col gap-y-8">
         <section class="bg-blue-300 w-full h-[200px] rounded-md"></section>
